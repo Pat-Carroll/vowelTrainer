dad1fba15d22aa0a780c7bdbec0b013803e79a4e
Patrick Carroll <pcc.carroll@gmail.com>
2016-01-08 13:41:34 +0100

    updating the calibrate template

--- client/templates/pages/calibrate.js
+++ client/templates/pages/calibrate.js
@@ -4,21 +4,36 @@ Template.calibrate.helpers({
     getCurrRecording: function () {
         return Session.get("currentRecording");
     },
+    hasValidCalibrationSentence:function(){
+        return SentenceProductions.findOne({owner: Meteor.userId(), sentenceId: this.sentenceId});
+    },
+    getSentence: function () {
+        return Sentences.findOne({number: this.sentenceId}).text;
+    },
 
 });
 
 Template.calibrate.events({
-    "click .js-nextStep": function (ev) {
-        console.log("Current Vowel " + this.vowel);
+    "click .submitSentence": function (ev) {
+        ev.preventDefault();
 
-        Meteor.call("saveSentenceSample", this.vowel, Session.get("currentRecording"), SentenceProductionsTypes.calibration);
+        console.log("Saved Sentence " + Sentences.findOne({number: this.sentenceId}).text);
+
+        // TODO include some return value in the saveSampleSentence to show success or failure
+        Meteor.call("saveSentenceSample", this.sentenceId, Session.get("currentRecording"), SentenceProductionsTypes.calibration);
 
         Session.set("currentRecording", undefined);
-        if (this.vowel < 10)
-            Router.go("/calibrate/" + (Number(this.vowel) + 1));
-        else
+
+    },
+    "click .js-nextStep": function (ev) {
+        if (this.sentenceId < 11)
+            Router.go("/calibrate/" + (Number(this.sentenceId) + 1));
+        else{
+            Meteor.call("calculateCalibration");
             Router.go("/");
-    }
+
+        }
+            }
 });
 
 Template.calibrate.onRendered(function () {
